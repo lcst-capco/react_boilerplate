864ab17fb1c4101f9f5849a7667e205810b00383
Lorina Castroverde <lorina.castroverde@capco.com>
2023-09-01 13:04:19 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const App = () => {
+    return (
+        <h3>This is a Boiler plate only</h3>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+    alias: {
+      react: path.join(__dirname, 'node_modules', 'react'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/, /build/],
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+      
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
